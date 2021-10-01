bded18b93c192edb7ab6702bd9cf9bce8a086202
Ricky-vitali <ricky.vitali28@gmail.com>
2021-10-01 00:02:22 -0300

    Realizacion del tp1

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+const API_KEY = '255ab47c33b843aa763667cb39ec7e59';
+
+    createContent(JSON.parse(localStorage.getItem('Data')));
+
+    document.querySelector('form').addEventListener('submit', e => {
+        e.preventDefault();
+        
+        let city = e.target.querySelector('input').value;
+        console.log(city);
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=sp`)
+        .then(res => res.json()
+    )
+        .then(res => {
+            console.log(res)
+            if(res.cod == 200){
+                createContent(res);
+
+                localStorage.setItem('Data', JSON.stringify(res));
+                document.querySelector('#error').style.display = 'none';
+            }else{
+                document.querySelector('#error').style.display = 'block';
+            }
+        });
+    });
+
+function createContent (content) {
+    document.querySelector('#cityname').innerHTML = content.name;
+    document.querySelector('#now').innerHTML = content.main.temp;
+    document.querySelector('#feel').innerHTML = content.main.feels_like;
+    document.querySelector('#max').innerHTML = content.main.temp_max;
+    document.querySelector('#min').innerHTML = content.main.temp_min;
+    document.querySelector('#speed').innerHTML = content.wind.speed;
+    document.querySelector('#humidity').innerHTML = content.main.humidity;
+    document.querySelector('#pressure').innerHTML = content.main.pressure;
+    document.querySelector('#weather').innerHTML = content.weather[0].description;
+    document.querySelector('.answer').style.display = 'block';
+}
